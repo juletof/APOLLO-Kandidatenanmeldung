7769a1592cee17dbd590e59838bfd17be13849bd
Robert Mischke <robertmischke@gmail.com>
2012-09-12 01:00:33 +0200

    (wip) Anmeldung

--- src/Frontend.Web/Controllers/Account/AnmeldungModel.cs
+++ src/Frontend.Web/Controllers/Account/AnmeldungModel.cs
@@ -1,8 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using ApolloDb;
 
 public class AnmeldungModel
 {
+    public AnmeldungModel()
+    {
+        Familienname = "asdfasdf";
+        UniOps = Universitaeten.ToSelectItems();
+    }
+
+    [Required(ErrorMessage = "* Pflichtfeld")]
+    public string Familienname { get; set; }
+
+    [Required(ErrorMessage = "* Pflichtfeld")]
+    public string Vorname { get; set; }
+
+    [Required(ErrorMessage = "* Pflichtfeld")]
+    public DateTime Geburtsdatum{ get; set; }
+
+    [Required(ErrorMessage = "* Pflichtfeld")]
+    public string Inlandspass { get; set; }
+
+    public string Mobilfunknummer { get; set; }
+
+    public string UniVal;
+    public IList<SelectListItem> UniOps;
+
 }
