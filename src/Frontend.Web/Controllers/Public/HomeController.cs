19edc301c6ba07258c57069c83422634776bcfa5
Robert Mischke <robertmischke@gmail.com>
2012-09-23 18:15:35 +0200

    Korrekte Buttons auf InformationSeite

---
+++ src/Frontend.Web/Controllers/Public/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Frontend.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Information()
+        {
+            return View(new InformationModel{ZurueckStartseite = true});
+        }
+
+        [HttpGet]
+        public ActionResult Information(string id)
+        {
+            if (id == "vonRegistrierung")
+                return View(new InformationModel { ZurueckRegistrierung = true });
+
+            if (id == "vonPersoenlicherBereich")
+                return View(new InformationModel { ZurueckPersoenlicherBereich = true });
+
+            return View(new InformationModel { ZurueckStartseite = true });
+        }
+
+        public ActionResult Information_Praktikum()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Frontend.Web/Controllers/Public/InformationModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class InformationModel
+{
+    public bool ZurueckStartseite;
+    public bool ZurueckPersoenlicherBereich;
+    public bool ZurueckRegistrierung;
+}
