e7737fcf2fceddf308fc4579dd9135c5b3b4eaa6
Robert Mischke <robertmischke@gmail.com>
2014-09-20 15:19:14 +0200

    show inactive universities selectively

--- src/Core/Domain/Kandidat/Lists/Model/ListItem.cs
+++ src/Core/Domain/Kandidat/Lists/Model/ListItem.cs
@@ -10,12 +10,14 @@ namespace ApolloDb
         public int Key;
         public string Deutsch;
         public string Russisch;
+        public bool IstAktiv = true;
 
-        public ListItem(int key, string deutsch = "", string russisch = "")
+        public ListItem(int key, string deutsch = "", string russisch = "", bool istAktiv = true)
         {
             Key = key;
             Deutsch = deutsch;
             Russisch = russisch;
+            IstAktiv = istAktiv;
         }
     }
 }
